4218a645a0824590eb747dfdcb04565c6eef53d9
Pyke Kaisora <pykekaisora@vivaldi.net>
2026-03-12 10:35:04 +0800

    update number to docker build script

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,6 +1,6 @@
 docker build . \
     -t telur.penyahpepijat.com/enigma/quivitech/inventory-management:latest \
-    -t telur.penyahpepijat.com/enigma/quivitech/inventory-management:0.0.1 \
+    -t telur.penyahpepijat.com/enigma/quivitech/inventory-management:0.0.2 \
     -t telur.penyahpepijat.com/enigma/quivitech/inventory-management:release
 echo "#  Pushing Container #"
 docker push telur.penyahpepijat.com/enigma/quivitech/inventory-management -a
