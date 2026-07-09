5e7fc62211c6116c92c93de10d66801237040500
Pyke Kaisora <pykekaisora@vivaldi.net>
2026-07-09 10:28:40 +0800

    final by iqbal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -313,10 +313,7 @@
         </li>
 
         <hr class="sidebar-divider">
-        <div class="version" style="padding:10px; font-size: 10px;">Version @{{ env('SOFTWAREVERSION')}} <br>By Enigma Code Solution1</div>
-        <div class="version" style="padding:10px; font-size: 10px;">Version {{ env('SOFTWAREVERSION')}} <br>By Enigma Code Solution2</div>
-        <div class="version" style="padding:10px; font-size: 10px;">Version {!! env('SOFTWAREVERSION')!!} <br>By Enigma Code Solution3</div>
-        <div class="version" style="padding:10px; font-size: 10px;">Version <?php echo env('SOFTWAREVERSION')?> <br>By Enigma Code Solution4</div>
+        <div class="version" style="padding:10px; font-size: 10px;">Version {{ env('SOFTWAREVERSION')}} <br>By Enigma Code Solution</div>
       </ul>
 
       <div id="content-wrapper" class="d-flex flex-column">
