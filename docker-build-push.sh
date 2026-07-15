7e82ff525e735e01d81d1031521aa86311071485
Pyke Kaisora <pykekaisora@vivaldi.net>
2026-07-15 23:16:43 +0800

    changed back to release tag in docker compose and rearrange loading .env and added docker image rm for cleanup

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,7 +1,7 @@
 #export SOFTWAREVERSION=0.0.3
 #source .env
-composer install && npm install && npm run dev
 . ./.env
+composer install && npm install && npm run prod
 echo "**********************************"
 echo "* Version is " $SOFTWAREVERSION " *"
 echo "**********************************"
@@ -11,11 +11,11 @@ docker build . \
     -t telur.enigmacode.com.my/enigma/quivitech/inventory-management:${SOFTWAREVERSION}
 echo "#  Pushing Container #"
 docker push telur.enigmacode.com.my/enigma/quivitech/inventory-management -a
-# docker image rm telur.enigmacode.com.my/enigma/quivitech/inventory-management:latest \
-#     telur.enigmacode.com.my/enigma/quivitech/inventory-management:release \
-#     telur.enigmacode.com.my/enigma/quivitech/inventory-management:${SOFTWAREVERSION}
-echo "# updating docker #"
 docker compose down
+docker image rm telur.enigmacode.com.my/enigma/quivitech/inventory-management:latest \
+    telur.enigmacode.com.my/enigma/quivitech/inventory-management:release \
+    telur.enigmacode.com.my/enigma/quivitech/inventory-management:${SOFTWAREVERSION}
+echo "# updating docker #"
 docker compose pull
 docker compose up -d
 echo "# finish #"
